746491ddace1ec12570aba40bd0dc5f7c85fd53f
Chris Harrington <ceharrin@icloud.com>
2025-03-02 17:32:47 -0600

    Completed Chap 4

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,36 @@
 import {StyleSheet, View} from "react-native";
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import {useState} from "react";
 
 const PlaceHolderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images"],
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+        } else {
+            alert("No image selected");
+        }
+    }
+
     // @ts-ignore
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceHolderImage}/>
+                <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage}/>
             </View>
             <View style={styles.footerContainer}>
-                <Button label={"Choose a photo"} theme={"primary"}/>
+                <Button label={"Choose a photo"} theme={"primary"} onPress={pickImageAsync}/>
                 <Button label={"Use this photo"}/>
             </View>
         </View>
--- components/Button.tsx
+++ components/Button.tsx
@@ -3,10 +3,11 @@ import {FontAwesome} from "@expo/vector-icons";
 
 type Props = {
     label: string;
-    theme: 'primary';
+    theme?: 'primary';
+    onPress?: () => void;
 };
 
-export default function Button({label, theme}: Props) {
+export default function Button({label, theme, onPress}: Props) {
     if (theme === 'primary') {
         return (
             <View style={[
@@ -14,8 +15,7 @@ export default function Button({label, theme}: Props) {
                 {borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18},
             ]}>
                 <Pressable
-                    style={[styles.button, {backgroundColor: "#fff"}]}
-                    onPress={() => alert("Button pressed")}>
+                    style={[styles.button, {backgroundColor: "#fff"}]} onPress={onPress}>
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
                     <Text style={[styles.buttonLabel, {color: '#25292e'}]}>{label}</Text>
                 </Pressable>
